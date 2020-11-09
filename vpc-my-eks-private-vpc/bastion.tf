8c61677cdce0db4e59e4f182948434c46fef50a5
Milan Das <milan@ulap.co>
2020-11-08 19:57:34 -0500

    removed tfstate

--- vpc-my-eks-private-vpc/bastion.tf
+++ vpc-my-eks-private-vpc/bastion.tf
@@ -1,3 +1,15 @@
+locals {
+  user_data = <<EOF
+#!/bin/bash
+echo "Hello Terraform!"
+cd /tmp
+curl -O https://releases.hashicorp.com/terraform/0.13.5/terraform_0.13.5_linux_amd64.zip
+unzip terraform_0.13.5_linux_amd64.zip
+chmod 755 terraform
+mv terraform /usr/local/bin/
+rm -rf /tmp/*
+EOF
+}
 module "ec2_cluster" {
   source                 = "terraform-aws-modules/ec2-instance/aws"
   version                = "~> 2.0"
@@ -11,6 +23,7 @@ module "ec2_cluster" {
   monitoring             = true
   vpc_security_group_ids = [module.ssh_sg.this_security_group_id]
   subnet_id              = module.my_eks_private_vpc.public_subnets[0]
+  user_data_base64 = base64encode(local.user_data)
 
   tags = {
     Terraform   = "true"
